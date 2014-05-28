11c56eefee68a28b07f3196f157915eeabe8fb2e
Philipp Haller <hallerp@gmail.com>
2014-05-28 10:23:15 +0200

    Expand test

--- src/test/scala/selfassembly/person/PersonB.scala
+++ src/test/scala/selfassembly/person/PersonB.scala
@@ -1,8 +1,10 @@
 package person
 
-case class Firefighter(n: String, a: Int, s: Int)
-  extends Person {
+class Volunteer(n: String, a: Int, s: Int) extends Person {
   def name = n
   def age = a
   def since = s
 }
+
+case class Firefighter(n: String, a: Int, s: Int)
+  extends Volunteer(n, a, s)
