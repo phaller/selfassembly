b3f85fb22506b5d82b21a94abeb0f987ed5528aa
Philipp Haller <hallerp@gmail.com>
2014-05-31 18:40:55 +0200

    Make PBuilderReader exact copy from scala-pickling

--- src/main/scala/selfassembly/examples/pickling/PBuilderReader.scala
+++ src/main/scala/selfassembly/examples/pickling/PBuilderReader.scala
@@ -1,8 +1,8 @@
-/*
- * Copyright (C) 2014 LAMP/EPFL
- * Copyright (C) 2014 Typesafe Inc. <http://www.typesafe.com>
- */
 package selfassembly.examples.pickling
+
+import scala.language.experimental.macros
+
+import scala.reflect.runtime.universe._
 
 trait Hintable {
   def hintTag(tag: FastTypeTag[_]): this.type
@@ -23,3 +23,22 @@ trait PBuilder extends Hintable {
   def endCollection(): Unit
   def result(): Pickle
 }
+
+trait PReader extends Hintable {
+  def mirror: Mirror
+  def beginEntry(): FastTypeTag[_]
+  def beginEntryNoTag(): String
+  def atPrimitive: Boolean
+  def readPrimitive(): Any
+  def atObject: Boolean
+  def readField(name: String): PReader
+  def endEntry(): Unit
+  def beginCollection(): PReader
+  def readLength(): Int
+  def readElement(): PReader
+  def endCollection(): Unit
+  def unpickle[T]: T = macro Compat.UnpickleMacros_readerUnpickle[T]
+  def unpickleTopLevel[T]: T = macro Compat.UnpickleMacros_readerUnpickleTopLevel[T]
+}
+
+case class PicklingException(msg: String) extends Exception(msg)
